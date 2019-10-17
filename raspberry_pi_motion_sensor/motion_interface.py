366185d1ffdc5f2a433c1ad2552a0a2b46c278fd
Curtis Woodworth <woodworthc10@gmail.com>
2019-10-17 09:56:55 -0400

    Added queue and pins to __init__ and updated doc

--- raspberry_pi_motion_sensor/motion_interface.py
+++ raspberry_pi_motion_sensor/motion_interface.py
@@ -10,19 +10,27 @@ class MotionPir:
     when armed.
     """
 
-    def __init__(self):
-        """ When MotionPir is created it will be inactive, self.armed set to false"""
+    def __init__(self, queue, channel_num):
+        """ When MotionPir is created it will be inactive, self.armed set to false.
+
+        Queue from main is given to object for passing of events happening in second thread.
+        channel_num is the Broadcom SOC channel number used for input from the PIR sensor.
+        """
 
         self.armed = False
+        self.queue = queue
+        self.channel_num = channel_num
 
     def motion_callback(self, channel):
         """channel argument is for receiving GPIO input.
 
         Function runs on different thread than main.
-        When object is 'armed' / True, this function will be called anytime the Pir sensor is activated.
+        When object (self.armed) is 'armed' / True, this function will be called anytime the Pir sensor is activated.
+        Gives main driver ability to see callback from different thread.
         """
 
         # Here, alternatively, an application / command can be started
+        self.queue.put(True)
         print("Movement Detected")
 
     def set_armed(self):
@@ -48,14 +56,18 @@ class MotionPir:
     def monitor(self):
         """GPIO is set up to manage callback on second thread to run motion_callback() in response to a rising edge"""
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(23, GPIO.IN)
-        GPIO.add_event_detect(23, GPIO.RISING, callback=self.motion_callback)
+        GPIO.setmode(
+            GPIO.BCM
+        )  # use GPIO.setmode(GPIO.board) for using pin numbers
+        GPIO.setup(self.channel_num, GPIO.IN)
+        GPIO.add_event_detect(
+            self.channel_num, GPIO.RISING, callback=self.motion_callback
+        )
 
     def stop_monitor(self):
         """Will stop callbacks from motion_pir"""
 
-        GPIO.remove_event_detect(23)
+        GPIO.remove_event_detect(self.channel_num)
 
 
 # TODO - make functions to set up GPIO pin settings and possibly initialize within __init__
