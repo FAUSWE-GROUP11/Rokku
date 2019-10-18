12d8ed6bc39c5323f6022e1a2ef0e0483ea52783
Fanchen Bao <jonathonbao@gmail.com>
2019-10-18 13:16:59 -0400

    Add new test case for logger

--- pi_to_pi/tests/test_mqtt_client.py
+++ pi_to_pi/tests/test_mqtt_client.py
@@ -2,6 +2,8 @@ from .. import publisher, subscriber
 import json
 from time import sleep
 from multiprocessing import Process, Queue
+import re
+import os
 
 
 def test_pub_sub():
@@ -15,7 +17,7 @@ def test_pub_sub():
 
     # publisher
     pub = publisher.Publisher("pub_test")
-    pub.publish(json.dumps({"intercom": "ON"}))  # publish a dict
+    pub.publish(json.dumps({"intercom": 1}))  # publish a dict
 
     # wait for subscriber to receive the msg
     while q.empty():
@@ -27,4 +29,18 @@ def test_pub_sub():
     sub.close()
     pub.close()
     # Test
-    assert json.loads(q.get())["intercom"] == "ON"
+    assert json.loads(q.get())["intercom"] == 1
+
+
+def test_logger():
+    os.remove("mqtt.log")  # remove any old logs
+    test_pub_sub()  # this produces a new log
+    with open("mqtt.log", "r") as f_obj:
+        data = f_obj.read()
+    num_success = len(re.findall(r"successful", data))  # should be 2
+    num_closed = len(re.findall(r"closed", data))  # should be 2
+    num_msg = len(re.findall(r"{\"intercom\": 1}", data))  # should be 2
+    # Test
+    assert sum([num_success, num_closed, num_msg]) == 6
+    # clean up
+    os.remove("mqtt.log")
