35ab3313e68844f719c0b7893658bfdbf8f94fb3
Fanchen Bao <jonathonbao@gmail.com>
2019-10-18 15:23:37 -0400

    Add more comments

--- pi_to_pi/tests/communication_sample.py
+++ pi_to_pi/tests/communication_sample.py
@@ -14,16 +14,20 @@ This code serves as an example for communication between two devices via mqtt.
 This file is NOT supposed to be tested by `pytest`
 """
 
+# For testing purpose only
 try:
     os.remove("mqtt.log")
 except Exception:
     pass
 
-# Instantiate publisher
+# Instantiate publisher, specify which topic to publish to.
+# This publisher can ONLY publish to the topic given at instantiation.
 pub = publisher.Publisher(topic="Rokku/mac_to_rpi")
 
-# Instantiate subscriber
+
 q = Queue()
+# Instantiate subscriber, specify which topic to subscribe to.
+# This subscriber can ONLY subscribe to the topic given at instantiation.
 sub = subscriber.Subscriber(q, topic="Rokku/rpi_to_mac")
 # listen in a separate process
 child_proc = Process(target=sub.start_listen, args=())
@@ -31,14 +35,16 @@ child_proc.start()
 
 
 counter = 0
-# main loop
+# main loop to publish 5 messages while also printing out any messages received
 while counter <= 10:
     if not q.empty():
-        print(q.get())
+        print(q.get())  # message received is retrieved from the queue
     else:
         pub.publish(f"hello from macbook {counter}")
         sleep(2)
         counter += 1
+
+# gracefully end all processes
 child_proc.terminate()
 child_proc.join()
 sub.close()
