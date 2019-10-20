5e6aa9398a65aab7118a63ea6dbce31459d36897
Curtis Woodworth <cwoodworth2015@fau.edu>
2019-10-19 23:22:40 -0400

    added more tests

--- raspberry_pi_motion_sensor/tests/test_motion_interface.py
+++ raspberry_pi_motion_sensor/tests/test_motion_interface.py
@@ -1,6 +1,8 @@
 import sys
+from pathlib import Path
 
 sys.path.append("/home/pi/github-repos/Rokku/raspberry_pi_motion_sensor")
+sys.path.append(str(Path.cwd().resolve().parent))
 from motion_interface import MotionPir
 from queue import Queue
 
@@ -18,10 +20,27 @@ class TestMotionPir:
     def test_init_3(self):
         assert motion.armed is False
 
+    def test_set_disarmed(self):
+        motion.set_armed()
+        motion.set_disarmed()
+        assert motion.armed is False
+
     def test_set_armed(self):
+        motion.set_disarmed()
         motion.set_armed()
         assert motion.armed is True
 
-    # TODO - def test_callback(self):
+    def test_motion_callback(self):
+        motion.motion_callback(23)
+        result = motion.queue.get()
+        assert result is True
 
-    # TODO - def test_monitor(self):
+    def test_get_state(self):
+        motion.set_disarmed()
+        motion.set_armed()
+        result = motion.get_state()
+        assert result is True
+
+    def test_get_state2(self):
+        result = motion.get_state()
+        assert result is True
