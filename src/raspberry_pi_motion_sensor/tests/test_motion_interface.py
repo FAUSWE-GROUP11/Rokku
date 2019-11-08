8fad87176992cb26d62aa6da6a129f8a1be1af3c
Curtis Woodworth <cwoodworth2015@fau.edu>
2019-11-08 10:39:29 -0500

    Added tests for motion interface

---
+++ src/raspberry_pi_motion_sensor/tests/test_motion_interface.py
@@ -0,0 +1,48 @@
+import sys
+from multiprocessing import Queue
+
+import fake_rpi
+
+from .. import motion_interface
+
+sys.modules["RPi"] = fake_rpi.RPi
+sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO
+
+queue = Queue()
+motion = motion_interface.MotionPir(queue, 23)
+
+
+class TestMotionPir:
+    def test_init(self):
+        assert motion.channel_num == 23
+
+    def test_init_2(self):
+        assert motion.queue.qsize() == 0
+
+    def test_init_3(self):
+        assert motion.armed is False
+
+    def test_set_disarmed(self):
+        motion.set_armed()
+        motion.set_disarmed()
+        assert motion.armed is False
+
+    def test_set_armed(self):
+        motion.set_disarmed()
+        motion.set_armed()
+        assert motion.armed is True
+
+    def test_motion_callback(self):
+        motion.motion_callback(23)
+        result = motion.queue.get()
+        assert result is True
+
+    def test_get_state(self):
+        motion.set_disarmed()
+        motion.set_armed()
+        result = motion.get_state()
+        assert result is True
+
+    def test_get_state2(self):
+        result = motion.get_state()
+        assert result is True
