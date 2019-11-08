f26cb68234d16344a71be5b629384aab87d5f075
Curtis Woodworth <cwoodworth2015@fau.edu>
2019-11-08 13:33:04 -0500

    formatting

--- src/raspberry_pi_motion_sensor/tests/test_motion_interface.py
+++ src/raspberry_pi_motion_sensor/tests/test_motion_interface.py
@@ -3,11 +3,10 @@ from multiprocessing import Queue
 
 import fake_rpi
 
-from .. import motion_interface
-
 sys.modules["RPi"] = fake_rpi.RPi
 sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO
 
+from .. import motion_interface
 
 queue = Queue()
 motion = motion_interface.MotionPir(queue, 23)
