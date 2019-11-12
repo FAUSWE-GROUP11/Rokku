7ffeadba04fc2a154bc273319d929ed4bd428eb9
Curtis Woodworth <cwoodworth2015@fau.edu>
2019-11-12 17:25:42 -0500

    behaviors modified to reply with correct responses. Formatting.

--- src/raspberry_pi_driver/behaviors.py
+++ src/raspberry_pi_driver/behaviors.py
@@ -1,5 +1,5 @@
 import json
-from time import sleep
+
 from ..raspberry_pi_motion_sensor.motion_interface import MotionPir
 
 
@@ -12,23 +12,29 @@ def motion(pub, flag) -> None:
         sensor.set_disarmed()
     pub.publish(json.dumps(["motion", sensor.get_state()]))
 
+
 def intercom(pub, flag) -> None:
     """Behavior that will connect or disconnect rpi_out with mumble client."""
     if flag is True:
         pass  # set up intercom for rpi_out
+        pub.publish(json.dumps(["intercom", True]))
     else:
         pass  # disconnect intercom
-    pub.publish(json.dumps(["intercom", False]))  # Change False to state of intercom
+        pub.publish(json.dumps(["intercom", False]))
+
 
 def record(pub, flag) -> None:
     """Behavior that will record a video, upload it to YouTube, and publish an URL to rpi_in."""
     if flag is True:
         # Start recording.
-        pub.publish(json.dumps(["record", True]))  # Will change button color, assuming video is being recorded.
+        pub.publish(
+            json.dumps(["record", True])
+        )  # Will change button color, assuming video is being recorded.
         # Once recording is over and YouTube gives link to playlist
-        pub.publish(json.dumps(["yt_playlist_link"]))
+        pub.publish(json.dumps(["yt_playlist_link", True]))
     else:
         pub.publish(json.dumps(["record", False]))  # Something went wrong
+
 
 def alarm(pub, flag) -> None:
     """Behavior that will set off alarm on rpi_out."""
