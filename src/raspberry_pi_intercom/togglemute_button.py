4611c47102819001e6c54691f7119887f094ddd2
Fanchen Bao <jonathonbao@gmail.com>
2019-11-24 18:24:43 -0500

    Change togglemute LED indicator to GPIO26

--- src/raspberry_pi_intercom/togglemute_button.py
+++ src/raspberry_pi_intercom/togglemute_button.py
@@ -15,25 +15,26 @@ def togglemute(logger: logging.Logger) -> None:
     Mumble client mute when button is released (LED light also off).
     """
     GPIO.setmode(GPIO.BCM)
-
+    input_pin = 16
+    output_pin = 26
     # input pin connected to button. Button is pull up, meaning button press
     # sends logic low, while button release high
-    GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     # Output pin connected to LED. High leads to LED on, while low off.
-    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(output_pin, GPIO.OUT)
     mute: bool = True  # flag
 
     try:
         while True:
             input_state = GPIO.input(16)
             if input_state:  # input high, button released
-                GPIO.output(12, GPIO.LOW)  # turn off LED
+                GPIO.output(output_pin, GPIO.LOW)  # turn off LED
                 if not mute:
                     subprocess.run(shlex.split("mumble rpc mute"))  # mute
                     mute = True
                     logger.debug("Mumble client MUTE")
             else:  # input low, button pressed
-                GPIO.output(12, GPIO.HIGH)  # turn on LED
+                GPIO.output(output_pin, GPIO.HIGH)  # turn on LED
                 if mute:
                     subprocess.run(shlex.split("mumble rpc unmute"))  # unmute
                     mute = False
